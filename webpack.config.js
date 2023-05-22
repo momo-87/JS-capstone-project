9c74f9916555787c7abd639bd7b3fe6e0dd9292b
Momo-87 <momochristian87@gmail.com>
2023-05-22 21:50:33 +0100

    Configuring linter controls

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,38 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-mode: "development",
-entry: './src/index.js',
-devServer: {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
     static: './dist',
-},
-plugins: [
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
+      template: './src/index.html',
     }),
-],
-output: {
+  ],
+  output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-},
-module: {
+  },
+  module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name]-[hash][ext]',
         },
-        {
-            test: /\.html$/i,
-            use: 'html-loader',
-        },
-        {
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/i,
-            type: 'asset/resource',
-            generator: {
-                filename: 'assets/[name]-[hash][ext]',
-            },
-        },
+      },
     ],
-},
-}
+  },
+};
