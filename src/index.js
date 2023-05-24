3ee4b64e584cb92748b2ea6f832f5fb3eec4186c
Momo-87 <momochristian87@gmail.com>
2023-05-24 02:51:47 +0100

    implement add new like feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 // import style from style.css file (required)
 import './style.css';
 
-// import populatedishes function
-import populatedishes from './modules/functions.js';
+// import the required functions from module
+import {
+  getMealData, populatedishes, addLike, getLikesData, diplayLikes,
+} from './modules/functions.js';
 
-populatedishes();
+// Getting data from the theMealDB API
+const mealData = await getMealData();
+
+// Getting data from the Involvement API function
+const likesData = await getLikesData();
+
+// Populating items in the home page
+populatedishes(mealData);
+
+// Diplaying likes data in the home page
+mealData.forEach((element) => {
+  diplayLikes(likesData, `M${element.idMeal}`);
+});
+
+const foodListSection = document.querySelector('.food-list');
+foodListSection.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target && e.target.matches('i.like')) {
+    const targetId = e.target.id;
+    await addLike(targetId);
+    const likesData = await getLikesData();
+    diplayLikes(likesData, targetId);
+  }
+});
--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,18 +1,21 @@
-// Get data from API and render the DOM
-const populatedishes = async () => {
+// Getting data from the theMealDB API function
+export const getMealData = async () => {
   const request = new Request('https://themealdb.com/api/json/v1/1/filter.php?a=Canadian');
   const response = await fetch(request);
   const data = await response.json();
-  console.log(data.meals);
+  return data.meals;
+};
+
+// Populating items in the DOM function
+export const populatedishes = async (mealData) => {
   const foodListSection = document.querySelector('.food-list');
-  // Create foodCard and append it to foodListSection
-  data.meals.forEach((element) => {
+  mealData.forEach((element) => {
     const foodCard = document.createElement('div');
     foodCard.classList.add('food-card', 'flex-column');
     foodCard.innerHTML = `<div class = 'image-box'><img src = '${element.strMealThumb}' alt = 'meal'></div>
                           <div class = 'flex-row meal-title-and-interactions-box'>
                             <h2 class = 'meal-name'>${element.strMeal}</h2>
-                            <span><i class="fa-sharp fa-solid fa-heart" style="color: #8b4513;"></i></span><span class = 'likes'>1000 likes</span>
+                            <i id ="M${element.idMeal}" class="fa-sharp fa-solid fa-heart heart" style="color: #8b4513;"></i><span id ="L${element.idMeal}" class = "likes">0 likes</span>
                           </div>
                           <div class = "buttons-box flex-row">
                             <button class = 'comment'>Comments</button>
@@ -23,4 +26,45 @@ const populatedishes = async () => {
     foodListSection.appendChild(foodCard);
   });
 };
-export default populatedishes;
+
+// Create new app function
+export const newApp = async () => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/');
+  await fetch(request, {
+    method: 'POST',
+  });
+};
+
+// Add likes to API function
+export const addLike = async (targetId) => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R20mJzx45L3RyqatiuEZ/likes/');
+  await fetch(request, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: targetId,
+    }),
+  });
+};
+
+// Getting data from the Involvement API function
+export const getLikesData = async () => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R20mJzx45L3RyqatiuEZ/likes/');
+  const response = await fetch(request);
+  const data = await response.json();
+  return data;
+};
+
+// Diplaying likes data in the home page function
+export const diplayLikes = async (likesData, itemId) => {
+  likesData.forEach((element) => {
+    if (element.item_id === itemId) {
+      // Replace M from the i tag id by L for obtaining the span id
+      const likeId = itemId.replace('M', 'L');
+      const span = document.querySelector(`#${likeId}`);
+      span.textContent = `${element.likes} likes`;
+    }
+  });
+};
